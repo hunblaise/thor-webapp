160c3418a277e015c7a10eb62d175e4b4aa9d7fd
hunblaise <Hajdu.Balazs.Donat@stud.u-szeged.hu>
2016-04-04 23:16:43 +0200

    @sensor css.

---
+++ src/main/webapp/resources/js/map/sensor-map.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    var mapContainer = $('#map');
+    var sensors = $('.sensor-module');
+
+    var map = new google.maps.Map(mapContainer.get(0), {
+        zoom: 12,
+        center: new google.maps.LatLng(sensors.first().data('lat'), sensors.first().data('lon')),
+        scrollwheel: false
+    });
+
+    sensors.each(function() {
+        var sensor = $(this);
+
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(sensor.data('lat'), sensors.data('lon')),
+            map: map
+        });
+    });
+
+});
